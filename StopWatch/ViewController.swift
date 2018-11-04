daaedd1dee17d14eff644fe5bc2239e8b7727bf1
fumiya-lit <kussan628.fumi1027@icloud.com>
2018-11-04 13:55:32 +0900

    first commit

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var label: UILabel!
+     @IBOutlet weak var stopBtn: UIButton!
+    
+    var count: Float = 0.0
+    var timer: Timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @objc func up(){
+        count += 0.01
+        label.text = String(format: "%.2f", count)
+    }
+    @objc func down(){
+        count -= 0.01
+        label.text = String(format: "%.2f", count)
+    }
+    
+    @IBAction func start(){
+        if !timer.isValid{
+            timer = Timer.scheduledTimer(
+                timeInterval: 0.01,
+                target: self,
+                selector: #selector(self.up),
+                userInfo: nil,
+                repeats: true
+            )
+        }
+    }
+    @IBAction func stop(){
+        
+        if timer.isValid{
+            timer.invalidate()
+            stopBtn.setTitle("Reset", for: .normal)
+        }else {
+            count = 0.0
+            label.text=String(format: "%.2f", count)
+            stopBtn.setTitle("Stop", for: .normal)
+        }
+        
+    }
+    
+    
+    
 }
 
